1738e15879046cecde3757541e6bff0fd52caf5a
APMAPM1 <113245435+APMAPM1@users.noreply.github.com>
2023-11-21 14:13:25 +0530

    First version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from './Components/Footer';
+import MainBody from './Components/MainBody';
+import StickyNavbar from './Components/StickyNavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <StickyNavbar />
+      <MainBody />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
